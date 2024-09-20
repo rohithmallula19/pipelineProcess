da7ba93605b27bb48009f31936b861753aa330bc
Rohith Mallula <71230484+rohithmallula19@users.noreply.github.com>
2024-09-20 11:48:47 -0400

    Update Pipelinetool.py

--- Pipelinetool.py
+++ Pipelinetool.py
@@ -35,7 +35,7 @@ def process_pipeline_data(input_file):
     new_data = pd.DataFrame(columns=column_names)
     new_data['SalesOrgCode'] = ['1030'] * len(organic_gains_filtered)
     new_data['DivCode'] = ['02'] * len(organic_gains_filtered)
-    new_data['ProfitCenter'] = organic_gains_filtered['Profit Center'].values
+    new_data['ProfitCenterCode'] = organic_gains_filtered['Profit Center #'].values
     new_data['CustomerCode'] = organic_gains_filtered['Customer #'].values
     new_data['SalesDistrictCode'] = organic_gains_filtered['Sales District #'].values
     new_data['SalesRepName'] = organic_gains_filtered['Sales Rep'].values
@@ -56,7 +56,7 @@ def process_pipeline_data(input_file):
     new_data1 = pd.DataFrame(columns=column_names)
     new_data1['SalesOrgCode'] = ['1030'] * len(loss_filtered)
     new_data1['DivCode'] = ['02'] * len(loss_filtered)
-    new_data1['ProfitCenter'] = loss_filtered['Profit Center'].values
+    new_data1['ProfitCenterCode'] = loss_filtered['Profit Center #'].values
     new_data1['CustomerCode'] = loss_filtered['Customer #'].values
     new_data1['SalesDistrictCode'] = loss_filtered['Sales District #'].values
     new_data1['SalesRepName'] = loss_filtered['Sales Rep'].values
